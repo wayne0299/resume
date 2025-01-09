47fc25bfb5988d86bece74d74a93741ee53762b9
wayne0299 <wayne02990@gmail.com>
2025-01-09 22:16:19 +0900

    이력서를 작성하여 excel 파일로 만들어 보자.
    --
    제대로 된 이력서 양식으로 만들어 볼 수 있도록 하자.
    (할것좀 하고.)

---
+++ src/main/java/kr/excel/resume/ResumeView.java
@@ -0,0 +1,103 @@
+package kr.excel.resume;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ResumeView {
+
+    private Scanner scanner;
+
+    public ResumeView() {
+        scanner = new Scanner(System.in);
+    }
+
+    public PersonInfo inputPersonInfo() {
+        System.out.print("사진 파일명을 입력해 주세요 : ");
+        String photo = scanner.nextLine();
+
+        System.out.print("이름을 입력 해 주세요 : ");
+        String name = scanner.nextLine();
+
+        System.out.print("이메일을 입력 해 주세요 : ");
+        String email = scanner.nextLine();
+
+        System.out.print("주소를 입력 해 주세요 : ");
+        String address = scanner.nextLine();
+
+        System.out.print("전화번호를 입력 해 주세요 : ");
+        String phoneNumber = scanner.nextLine();
+
+        System.out.print("생년월일을 입력 해 주세요 : ");
+        String birthDate = scanner.nextLine();
+
+        return new PersonInfo(photo, name, email, address, phoneNumber, birthDate);
+    }
+
+    public List<Education> inputEducationList() {
+        List<Education> educationList = new ArrayList<>();
+
+        while (true) {
+            System.out.println("학력 정보를 입력해 주세요 :  ( 종료 : q )");
+            System.out.println("졸업년도 학교명 전공 졸업여부");
+
+            String input = scanner.nextLine();
+            if (input.equalsIgnoreCase("q")) {
+                break;
+            }
+
+            String[] tokens = input.split(" ");
+            if (tokens.length != 4) {
+                System.out.println("잘못된 입력입니다.");
+                continue;
+            }
+
+            String graduationYear = tokens[0];
+            String schoolName = tokens[1];
+            String major = tokens[2];
+            String graduationStatus = tokens[3];
+
+            educationList.add(new Education(graduationYear, schoolName, major, graduationStatus));
+        }
+        return educationList;
+    }
+
+    public List<Career> inputCareerList() {
+        List<Career> careerList = new ArrayList<>();
+
+        while (true) {
+            System.out.println("경력정보를 입력해 주세요 :  ( 종료 : q )");
+            System.out.println("근무기간 근무처 담당업무 근속연수");
+
+            String input = scanner.nextLine();
+            if (input.equalsIgnoreCase("q")) {
+                break;
+            }
+
+            String[] tokens = input.split(" ");
+            if (tokens.length != 4) {
+                System.out.println("잘못된 입력입니다.");
+                continue;
+            }
+
+            String workPeriod = tokens[0];
+            String companyName = tokens[1];
+            String jobTitle = tokens[2];
+            String employmentYears = tokens[3];
+
+            careerList.add(new Career(workPeriod, companyName, jobTitle, employmentYears));
+        }
+        return careerList;
+    }
+
+    public String inputSelfIntroduction(){
+        System.out.println("자기소개서를 입력 하세요 :  여러줄을 입력 하려면 빈 줄을 입력 하세요. ");
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while((line = scanner.nextLine()).trim().length() > 0 ){
+            sb.append(line).append("\n");
+        }
+        return sb.toString().trim();
+    }
+}
+
